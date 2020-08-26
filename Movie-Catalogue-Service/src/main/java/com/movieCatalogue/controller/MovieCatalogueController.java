fe6f6fd866e280b535b3d243fb7fb05d58719492
Aditya Raj Verma <adityarajverma@gmail.com>
2020-08-26 22:42:25 +0530

    Updated Movie Catalogue Service to accept new Movie Information

--- Movie-Catalogue-Service/src/main/java/com/movieCatalogue/controller/MovieCatalogueController.java
+++ Movie-Catalogue-Service/src/main/java/com/movieCatalogue/controller/MovieCatalogueController.java
@@ -3,19 +3,27 @@ package com.movieCatalogue.controller;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.movieCatalogue.model.CatalogueItem;
+import com.movieCatalogue.model.Info;
 
 @RestController
 @RequestMapping(value = "/moviecatalogue")
 public class MovieCatalogueController {
 
-	@GetMapping(value = "/{userId}")
-	public List<CatalogueItem> movieCatalogue(@PathVariable("userId") String userId) {
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping(value = "/{movieId}")
+	public List<CatalogueItem> movieCatalogue(@PathVariable("movieId") String movieId) {
+
+		Info info = restTemplate.getForObject("http://MOVIE-INFO-SERVICE/" + movieId, Info.class);
 
 		return Collections.singletonList(new CatalogueItem("Transformers", "Action", "8"));
 	}
