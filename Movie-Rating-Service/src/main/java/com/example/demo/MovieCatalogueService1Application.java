b635616ce0d80d83af28a684f08790ae46c881dc
Aditya Raj Verma <adityarajverma@gmail.com>
2020-08-26 15:16:04 +0530

    Init  3 Projects

---
+++ Movie-Rating-Service/src/main/java/com/example/demo/MovieCatalogueService1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogueService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogueService1Application.class, args);
+	}
+
+}
