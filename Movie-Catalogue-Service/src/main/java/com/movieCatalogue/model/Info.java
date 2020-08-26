cd8702e645c5393dc1eb3e36bd69e618f3cf8b24
Aditya Raj Verma <adityarajverma@gmail.com>
2020-08-26 22:43:32 +0530

    Added New Models on Movie Catalogue Service

---
+++ Movie-Catalogue-Service/src/main/java/com/movieCatalogue/model/Info.java
@@ -0,0 +1,87 @@
+package com.movieCatalogue.model;
+
+public class Info {
+
+	private String id;
+	private String original_language;
+	private String original_title;
+	private String overview;
+	private String release_date;
+	private String revenue;
+	private String status;
+	private String runtime;
+	private String homepage;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOriginal_language() {
+		return original_language;
+	}
+
+	public void setOriginal_language(String original_language) {
+		this.original_language = original_language;
+	}
+
+	public String getOriginal_title() {
+		return original_title;
+	}
+
+	public void setOriginal_title(String original_title) {
+		this.original_title = original_title;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+
+	public String getRelease_date() {
+		return release_date;
+	}
+
+	public void setRelease_date(String release_date) {
+		this.release_date = release_date;
+	}
+
+	public String getRevenue() {
+		return revenue;
+	}
+
+	public void setRevenue(String revenue) {
+		this.revenue = revenue;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getHomepage() {
+		return homepage;
+	}
+
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+
+}
---
+++ Movie-Catalogue-Service/src/main/java/com/movieCatalogue/model/Rating.java
@@ -0,0 +1,30 @@
+package com.movieCatalogue.model;
+
+public class Rating {
+
+	private String id;
+	private String rating;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public Rating(String id, String rating) {
+		super();
+		this.id = id;
+		this.rating = rating;
+	}
+
+}
